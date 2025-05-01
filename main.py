c94b497d59a3d7a3cb264827a92e936cada99d50
Lims-Paces <oluwapamilerin1234@gmail.com>
2025-05-01 12:20:31 +0100

    wip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import pyshorteners
 app = Flask(__name__)
  
 @app.route("/", methods=['POST', 'GET'])
@@ -6,6 +7,7 @@ def home():
   if request.method=="POST":
     url_received = request.form["url"]
     short_url = pyshorteners.Shortener().tinyurl.short(url_received)
+    print(shorten_url)
     return render_template("form.html", new_url=short_url, old_url=url_received)
   else:
     return render_template('form.html')
