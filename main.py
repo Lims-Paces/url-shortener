e0b5c0a1cf87cf11fbda797518303a61ba380229
tublian-ai-engineer <nilanjan+tos@tublian.com>
2025-05-01 14:48:52 +0000

    fix: proposing a solution for 4

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+
 from flask import Flask, render_template, request
 import pyshorteners
 app = Flask(__name__)
- 
+  
 @app.route("/", methods=['POST', 'GET'])
 def home():
   print('Preparing to execute')
   if request.method=="POST":
     url_received = request.form["url"]
-    short_url = pyshorteners.Shortener().tinyurl.short(url_received)
+    try:
+        short_url = pyshorteners.Shortener().tinyurl.short(url_received)
+    except Exception as e:
+        short_url = "Error generating short URL"
     print('executing')
-    print(shorten_url)
+    print(short_url)
     return render_template("form.html", new_url=short_url, old_url=url_received)
   else:
     print('in else block')
     return render_template('form.html')
- 
+  
 if __name__ == "__main__":
- app.run() 
+ app.run()
